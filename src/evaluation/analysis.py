9457ac25a8df5b9d498484fd79213c5b7a50ff6f
jerry-tl210 <40560663+jerry-tl210@users.noreply.github.com>
2020-08-30 11:01:23 +0800

    Add files via upload

--- src/evaluation/analysis.py
+++ src/evaluation/analysis.py
@@ -8,7 +8,6 @@ def get_analysis(data):
             out_string += q['QTEXT_CN'] + '\n'
             out_string += "answer:{}".format([ans['ATEXT_CN'] for ans in q['ANSWER']]) + '\n'
             out_string += "gold_SE:{}".format(q['SHINT_']) + '\n'
-            out_string += "answer_SE:{}".format(q['answer_sp']) + '\n'
             out_string += "predict_SE:{}".format(q['sp']) + '\n'
             out_string += '\n'
             all_set = set(q['SHINT_']) | set(q['sp'])
